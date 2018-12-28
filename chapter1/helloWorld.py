f8c1c23c2dd15d629432ba06d6480655486cd708
cartertrafton <cartertrafton@gmail.com>
2018-12-28 12:38:40 -0500

    updated chap 1

--- chapter1/helloWorld.py
+++ chapter1/helloWorld.py
@@ -1,17 +1,20 @@
-# 
 # The quintessential hello world
 # print(), input(), len()
 # str(), int(), float() (unused)
 
+# Printing
 print('Hello world')
 
+# Input
 print('What is your name?')
 myName = input()
 
+# Length 
 print('It is good to meet you ' + myName)
 print('The length of your name is: ')
 print(len(myName))
 
+# Type-changing
 print('What is your age?')
 myAge = input()
 print('You will be ' + str(int(myAge) + 1) + ' in a year.')
